bc3024b075c720eb76dfd13bded3ce82e21c4a84
Eric <ericlongz@ymail.com>
2021-06-27 12:33:05 +0700

    updated script (rules and symbol)

--- ConwaysGameOfLife.py
+++ ConwaysGameOfLife.py
@@ -16,16 +16,17 @@ for i in range(height):
 	for j in range(width):
 		value = random.randint(0,1)
 		if value == 0:
-			list_column.append("#")
+			list_column.append("*")
 		else:
 			list_column.append(" ")
 	nextcells.append(list_column)
 
-counter_saved_cells = 0
+#counter_saved_cells = 0
+
 #start loop
 while True:
 	#set how many step so we can save the cells
-	save_step = 3
+	#save_step = 3
 	
 	print("\n\n\n\n\n\n\n\n")
 	#print current cells
@@ -42,93 +43,120 @@ while True:
 			top = " "
 			bottom = " "
 			right = " "
+			toprig = " "
+			botrig = " "
 			left = " "
+			topleft = " "
+			botleft = " "
+			
 			if x == 0:
 				if y == 0:
 					bottom = current_cells[x+1][y+0]
 					right = current_cells[x+0][y+1]
-					#botrig = current_cells[x+1][y+1]
+					botrig = current_cells[x+1][y+1]
 				elif y == (width-1):
 					bottom = current_cells[x+1][y+0]
 					left = current_cells[x+0][y-1]
-					#botlef = current_cells[x+1][y-1]
+					botlef = current_cells[x+1][y-1]
 				else:
 					bottom = current_cells[x+1][y+0]
 					right = current_cells[x+0][y+1]
-					#botrig = current_cells[x+1][y+1]
+					botrig = current_cells[x+1][y+1]
 					left = current_cells[x+0][y-1]
-					#botlef = current_cells[x+1][y-1]
+					botlef = current_cells[x+1][y-1]
 			elif x == (height-1):
 				if y == 0:
 					top = current_cells[x-1][y+0]
 					right = current_cells[x+0][y+1]
-					#toprig = current_cells[x-1][y+1]
+					toprig = current_cells[x-1][y+1]
 				elif y == (width-1):
 					top = current_cells[x-1][y+0]
 					left = current_cells[x+0][y-1]
-					#toplef = current_cells[x-1][y-1]
+					toplef = current_cells[x-1][y-1]
 				else:
 					top = current_cells[x-1][y+0]
 					right = current_cells[x+0][y+1]
-					#toprig = current_cells[x-1][y+1]
+					toprig = current_cells[x-1][y+1]
 					left = current_cells[x+0][y-1]
-					#toplef = current_cells[x-1][y-1]
+					toplef = current_cells[x-1][y-1]
 			else:
 				if y == 0:
 					top = current_cells[x-1][y+0]
 					bottom = current_cells[x+1][y+0]
 					right = current_cells[x+0][y+1]
-					#toprig = current_cells[x-1][y+1]
-					#botrig = current_cells[x+1][y+1]
+					toprig = current_cells[x-1][y+1]
+					botrig = current_cells[x+1][y+1]
 				elif y == (width-1):
 					top = current_cells[x-1][y+0]
 					bottom = current_cells[x+1][y+0]
 					left = current_cells[x+0][y-1]
-					#toplef = current_cells[x-1][y-1]
-					#botleft = current_cells[x+1][y-1]
+					toplef = current_cells[x-1][y-1]
+					botleft = current_cells[x+1][y-1]
 				else:
 					top = current_cells[x-1][y+0]
 					bottom = current_cells[x+1][y+0]
 					right = current_cells[x+0][y+1]
-					#toprig = current_cells[x-1][y+1]
-					#botrig = current_cells[x+1][y+1]
+					toprig = current_cells[x-1][y+1]
+					botrig = current_cells[x+1][y+1]
 					left = current_cells[x+0][y-1]
-					#toplef = current_cells[x-1][y-1]
-					#botleft = current_cells[x+1][y-1]
+					toplef = current_cells[x-1][y-1]
+					botleft = current_cells[x+1][y-1]
 			
-			#
-			if top == "#":
+			#count counters
+			if top == "*":
 				counter = counter + 1
 			else:
 				counter = counter + 0
-			if bottom == "#":
+			if bottom == "*":
 				counter = counter + 1
 			else:
 				counter = counter + 0
-			if right == "#":
+			if right == "*":
 				counter = counter + 1
 			else:
 				counter = counter + 0
-			if left == "#":
+			if toprig == "*":
+				counter = counter + 1
+			else:
+				counter = counter + 0
+			if botrig == "*":
+				counter = counter + 1
+			else:
+				counter = counter + 0
+			if left == "*":
+				counter = counter + 1
+			else:
+				counter = counter + 0
+			if topleft == "*":
+				counter = counter + 1
+			else:
+				counter = counter + 0
+			if botleft == "*":
 				counter = counter + 1
 			else:
 				counter = counter + 0
 			
-			#
-			if counter > 2:
-				nextcells[x][y] = "#"
+			#Conway's Rules:
+			if current_cells[x][y] == "*":
+				if (counter == 2) or (counter == 3):
+					nextcells[x][y] = "*"
+				else:
+					nextcells[x][y] = " "
 			else:
-				nextcells[x][y] = " "
+				if counter == 3:
+					nextcells[x][y] = "*"
+				else:
+					nextcells[x][y] = " "
 				
-	if (counter_saved_cells != 0) and (counter_saved_cells % save_step == 0):
-		for x in range(height):
-			for y in range(width):
-				if (saved_cells[x][y] == " ") and (nextcells[x][y] == " "):
-					nextcells[x][y] = "#"
+	#if (counter_saved_cells != 0) and (counter_saved_cells % save_step == 0):
+	#	for x in range(height):
+	#		for y in range(width):
+	#			if (saved_cells[x][y] == " ") and (nextcells[x][y] == " "):
+	#				nextcells[x][y] = "#"
 		
-	if (counter_saved_cells % save_step) == 0:
-		saved_cells = copy.deepcopy(nextcells)
+	#if (counter_saved_cells % save_step) == 0:
+	#	saved_cells = copy.deepcopy(nextcells)
 		
-	counter_saved_cells = counter_saved_cells + 1
-	time.sleep(0.8)
+	#counter_saved_cells = counter_saved_cells + 1
+	time.sleep(1)
 	
