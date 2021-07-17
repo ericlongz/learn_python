7f59fbd7d7f50e2c5c601473c9f32e03c6be07c1
Eric <ericlongz@ymail.com>
2021-07-17 10:29:17 +0700

    simpler method

--- coin_flip_streaks.py
+++ coin_flip_streaks.py
@@ -4,23 +4,13 @@ import random
 numberOfStreaks = 0
 for experimentNumber in range(10000):
 	#Code that creates a list of 100 'heads' or 'tails' values.
-	ht_list = []
-	streak = 0
+	headTail = ""
 	for i in range(100):
-		ht_list.append(random.randint(0,1))
+		headTail = headTail + str(random.randint(0,1))
 
 	#Code that checks if there is a streak of 6 heads or tails in a row.
-		if i == 0:
-			pass
-		else:
-			if ht_list[i] == ht_list[i-1]:
-				streak = streak + 1
-			else:
-				streak = 0
-		
-		if streak == 6:
-			numberOfStreaks += 1
-			break
+	if ('000000' in headTail) or ('111111' in headTail):
+		numberOfStreaks += 1
 
 print('Chance of streak: %s%%' % (numberOfStreaks / 100))
 
