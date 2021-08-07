1bc1996900b405165da92bd2f19912af4fb0b00a
Eric <ericlongz@ymail.com>
2021-08-07 11:34:19 +0700

    test local API

--- learn_API1.py
+++ learn_API1.py
@@ -1,9 +1,20 @@
 import requests
 import json
+from flask import request
 
-url = "https://api.stackexchange.com/2.3/questions?fromdate=1601510400&todate=1630368000&order=desc&sort=activity&site=stackoverflow"
+#Test API Stackexchange
+#url = "https://api.stackexchange.com/2.3/questions?fromdate=1601510400&todate=1630368000&order=desc&sort=activity&site=stackoverflow"
 
-response = requests.get(url)
+#response = requests.get(url)
 
-for data in response.json()['items']:
-	print(data['tags'])
+#for data in response.json()['items']:
+#	print(data['tags'])
+
+#Test API local
+url = "http://127.0.0.1:5000/drinks"
+
+data_json = {'name': 'Whiskey', 'description': 'Whisky or whiskey is a type of distilled alcoholic beverage made from fermented grain mash. Various grains are used for different varieties, including barley, corn, rye, and wheat.'}
+
+response = requests.post(url, json=data_json)
+print(response)
+print(response.json())
