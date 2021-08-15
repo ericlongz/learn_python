34d1b37cdf7076dcd757b2f2f5f77f65f710b753
Eric <ericlongz@ymail.com>
2021-08-15 09:15:00 +0700

    ini test batch di windows

---
+++ printHelloWorld.bat
@@ -0,0 +1,9 @@
+@echo off
+
+rem title This is your first batch script!
+echo Welcome to batch scripting!
+
+rem kalau uda @echo off ga perlu pake @ lagi karena stepnya ga akan keprint
+py C:\Users\LENOVO\learn_python\hello_world.py %*
+
+pause
